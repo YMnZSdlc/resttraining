aa4ddf1e80b2c25dbe6c1f69ba8852dbb98e0f90
Michał Mielczarek <>
2018-09-01 20:30:33 +0200

    MM. Init

---
+++ src/main/java/pl/sda/sesttraining/MyMessage.java
@@ -0,0 +1,15 @@
+package pl.sda.sesttraining;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MyMessage {
+
+    private Integer id;
+    private String message;
+}
